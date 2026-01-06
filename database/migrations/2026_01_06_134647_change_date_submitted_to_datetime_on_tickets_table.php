8f13ba51c2e649413a6338150f3676fd953315e2
Jhune123 <rjdoctor@ksu.edu.ph>
2026-01-06 13:48:42 +0800

    change date submitted to datetime

---
+++ database/migrations/2026_01_06_134647_change_date_submitted_to_datetime_on_tickets_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->dateTime('date_submitted')->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('tickets', function (Blueprint $table) {
+            $table->date('date_submitted')->change();
+        });
+    }
+};
